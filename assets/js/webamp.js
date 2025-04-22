fff81b670b75c39f638ad60d5fae6707c4347fcc
jusjaeger <jusjaeger@student.ethz.ch>
2025-04-22 20:36:14 +0200

    added playlist buttons and fixed tags for disc3.2

--- assets/js/webamp.js
+++ assets/js/webamp.js
@@ -2,6 +2,7 @@ setTimeout(function() {
     document.querySelector('.loading-container').style.opacity = '0';
     document.querySelector('.logo-container').style.opacity = '1';
 
+    const Winamp = window.Webamp
     if(!Webamp.browserIsSupported()) {
         alert("Oh no! Webamp does not work!")
         throw new Error("What's the point of anything?")
@@ -20,9 +21,31 @@ setTimeout(function() {
         { url: `${disc31url}08.DWAMS-xX_angel_tearz_Xx.mp3` },
         { url: `${disc31url}09.Summoned_skull-Lust_and_Delirium.mp3` },
         { url: `${disc31url}10.MEKA-Love_Lies_Bleeding.mp3` },
-
-
     ]
+    const DISCBtn31 = document.getElementById('DISC31BTN');
+    function click31() {
+        webamp.setTracksToPlay(disc31tracks)
+    }
+    DISCBtn31.addEventListener('click', click31);
+
+
+    const disc32url = "assets/tracks/DISC3.2-RAPTURE/"
+    const disc32tracks = [
+        {url: `${disc32url}01.pixygarden-Floating_Angels.mp3` },
+        {url: `${disc32url}02.Helena_DLX-Cloud9.mp3` },
+        {url: `${disc32url}03.nabii-ALL_THE_WAY.mp3` },
+        {url: `${disc32url}04.earthangelbaby-you_remind_me.mp3` },
+        {url: `${disc32url}05.2040x2040-ERRORCODE2040.mp3` },
+        {url: `${disc32url}06.enable_secret-make_u_stay.mp3` },
+        {url: `${disc32url}07.dreamloader-eurolife.mp3` },
+        {url: `${disc32url}08.beansclub-still_thinking_bout_you.mp3` },
+    ]
+    const DISCBtn32 = document.getElementById('DISC32BTN');
+    function click32() {
+        webamp.setTracksToPlay(disc32tracks)
+    }
+    DISCBtn32.addEventListener('click', click32);
+
 
 
     const webamp = new Webamp({
