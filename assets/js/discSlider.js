b10f2bc23c690095034aa32071c4d5f353dca3a1
eu4i <t.cedervall@pm.me>
2025-04-25 18:12:34 +0200

    ok last one

--- assets/js/discSlider.js
+++ assets/js/discSlider.js
@@ -1,41 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     const swiper = new Swiper(".mySwiper", {
       effect: "cards",
-      grabCursor: true,
+      grabCursor: false,
       centeredSlides: true,
       loop: true,
-      slidesPerView: 2,
-      spaceBetween: 15,
       slideShadows: false,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+      mousewheel: true,
+      keyboard: true,
     });
   
-   
     document.querySelectorAll(".cdbutton").forEach((button) => {
       button.addEventListener("touchstart", (e) => {
         e.stopPropagation(); 
       });
     });
   
-
     document.querySelectorAll(".cdbutton").forEach((button) => {
       button.addEventListener("click", function (event) {
-        event.preventDefault(); // Prevent the default behavior
+        event.preventDefault();
         console.log(`Button ${button.id} clicked`);
   
-
         document.querySelectorAll(".cdbutton").forEach((btn) => {
           btn.classList.remove("spin");
         });
   
- 
         button.classList.add("spin");
       });
     });
