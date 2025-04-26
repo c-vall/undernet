43dba28e1f35bdd31b1c9cecd86ef01089163ebc
eu4i <t.cedervall@pm.me>
2025-04-26 17:25:59 +0200

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# UNDERNET PROTO WEBSITE
-
-## SUGGESTIONS/THINGS TO ADD
-
-* Music player
-* Store page
-* Artists page
-* Discography page
-* News feed / Social media posts page
--- assets/js/script.js
+++
@@ -1,4 +0,0 @@
-setTimeout(function() {
-    document.querySelector('.loading-container').style.opacity = '0';
-    document.querySelector('.logo-container').style.opacity = '1';
-}, 1500);
