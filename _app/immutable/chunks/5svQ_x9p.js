d4ec7598c1a3178861746f821e33ce1ca3e9b62c
eu4i <t.cedervall@pm.me>
2025-04-26 18:45:45 +0200

    Updates

--- _app/immutable/chunks/5svQ_x9p.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1l3t5sl)==null?void 0:s.base)??"/undernet";var t;const l=((t=globalThis.__sveltekit_1l3t5sl)==null?void 0:t.assets)??e;export{l as a,e as b};
