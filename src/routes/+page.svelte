1df685012d80a5302de30900213bdbbabdb20f38
eu4i <t.cedervall@pm.me>
2025-04-27 23:44:48 +0200

    UI update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,13 @@
   import WebampPlayer from '$lib/components/WebampPlayer.svelte';
   import Draggable from '$lib/components/Draggable.svelte';
 
-  const initialTracks = [
-    { url: '/tracks/DISC3.1-SURGE/01.Sienna_Sleep-SpectrumSetup420.mp3' },
-    { url: '/tracks/DISC3.1-SURGE/02.latex_fruit-jinXtyle.mp3' },
-  ];
+  let activeSection = 'player';
+
+  function setSection(section) {
+    activeSection = section;
+  }
 </script>
+
 
 <svelte:head>
   <title>under.net</title>
@@ -26,70 +28,68 @@
         <Logo/>
       </div>
       <div class="window" id="nextologo-container">
+        <ul class="nav-menu">
+          <li class="header-item">
+            <button class="nav-button" on:click={() => setSection('members')}></button>
+          </li>
+          <li class="header-item">
+            <button class="nav-button" on:click={() => setSection('player')}>󰓃</button>
+          </li>
+          <li class="header-item">
+            <button class="nav-button" on:click={() => setSection('merch')}>󰒚</button>
+          </li>
+          <li class="header-item">
+            <button class="nav-button" on:click={() => setSection('social')}>󰤉</button>
+          </li>
+          <li class="header-item">
+            <button class="nav-button" on:click={() => setSection('about')}>󰋼</button>
+          </li>
+        </ul>
       </div>
     </div>
-    <div class="window" id="music-container">
-      <div id="cdslider-container">
-        <CDSlider />
+
+  {#if activeSection === 'player'}
+    <div class="content-window" id="player-container">
+      <div class="window" id="video-container"></div>
+      <div class="lower-music-content">
+        <div id="cdslider-container">
+          <CDSlider />
+        </div>
+        <div class="window" id="track-container"></div>
       </div>
-      <div class="window" id="track-container">
+    </div>
+  {/if}
+  
+  {#if activeSection === 'members'}
+    <div class="content-window" id="members-container">
+      <div class="window" id="world-map">
+        <img src="https://t4.ftcdn.net/jpg/02/99/21/23/360_F_299212330_IJrlUJrSG0KGovF5nJKFgms3A8cvHDLQ.jpg" alt="World Map" />
       </div>
-    </div>      
+    </div>
+  {/if}
+
+  {#if activeSection === 'merch'}
+    <div class="content-window" id="merch-container">
+
+    </div>
+  {/if}
+  
+  {#if activeSection === 'social'}
+    <div class="content-window" id="social-container">
+
+    </div>
+  {/if}
+  
+  {#if activeSection === 'about'}
+    <div class="content-window" id="about-container">
+
+    </div>
+  {/if}
+    
   </section>
 </div>
 
 <style>
-.window {
-  background-image: url('/images/window_bg.png');
-  background-size: cover;
-  background-repeat: repeat;
-  border: ridge 4px #c0c0c0;
-  border-radius: 10px 6px 20px 6px;
-  outline: solid 4px black;
-  overflow: visible;
-  position: relative;
-}
 
-#main-container {
-  width: min(1100px);
-  height: min(90vh, 1000px);
-  max-width: 90%;
-  max-height: 90vh;
-  overflow: visible;
-}
-
-#logo-container {
-  background-image: url('/images/window_bg_light.png');
-  flex: 2;
-}
-
-#nextologo-container {
-  background-image: url('/images/background.png');
-  width: 100%;
-  flex: 1;
-}
-
-.header-container {
-  display: flex;
-  gap: 10px;
-  padding: 10px;
-}
-
-#music-container {
-  background-image: url('/images/background.png');
-  display: flex;
-  gap: 10px;
-  padding: 10px;
-  margin: 10px;
-}
-
-#cdslider-container {
-  flex: 1;
-}
-
-#track-container {
-  background-image: url('/images/window_bg_light.png');
-  flex: 2;
-}
 </style>
 
