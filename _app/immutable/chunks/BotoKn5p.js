34c653ccb50399d16bbc1b7830367a67e0d4f9a7
eu4i <t.cedervall@pm.me>
2025-04-28 20:23:53 +0200

    Updates

--- _app/immutable/chunks/BotoKn5p.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_osj2cx)==null?void 0:s.base)??"/undernet";var e;const a=((e=globalThis.__sveltekit_osj2cx)==null?void 0:e.assets)??t;export{a,t as b};
--- _app/immutable/entry/start.DrtNJjsN.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/idd1rZEA.js";export{o as load_css,r as start};
